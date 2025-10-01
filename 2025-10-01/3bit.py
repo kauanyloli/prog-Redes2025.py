c996fb481c18579bc362e95e4992ce966f4ffd6f
kauanyloli <kauanyyuno@gmail.com>
2025-10-01 18:04:05 -0300

    1

--- 2025-10-01/3bit.py
+++ 2025-10-01/3bit.py
@@ -1,4 +1,6 @@
 # Operações bit a bit (bitwise) com strings em Python
+import os
+
 strTexto_1 = 'Programação'
 strTexto_2 = 'desenvolver'
 
@@ -27,6 +29,18 @@ while intPosicao < len(strTexto_1):
    intPosicao += 1
 
 # Exibe os resultados
-print(f'Texto_1 & Texto_2......: {strResultadoAND}')
-print(f'Texto_1 | Texto_2......: {strResultadoOR}')
-print(f'Texto_1 ^ Texto_2......: {strResultadoXOR}')
+print(f'Texto_1 & Texto_2......: {strResultadoAND} -> {len(strResultadoAND)} caracteres')
+print(f'Texto_1 | Texto_2......: {strResultadoOR} -> {len(strResultadoOR)} caracteres')
+print(f'Texto_1 ^ Texto_2......: {strResultadoXOR} -> {len(strResultadoXOR)} caracteres')
+
+# Escrevendo em um arquivo
+strDir = os.path.dirname(__file__)
+strArquivo = os.path.join(strDir, 'resultado_operacoes.txt')
+arqSaida   = open(strArquivo, 'w', encoding='utf-8')
+arqSaida.write('Resultados das operações bit a bit (bitwise) com strings\n\n')
+arqSaida.write(f'Texto 1................: {strTexto_1}\n')
+arqSaida.write(f'Texto 2................: {strTexto_2}\n\n\n')
+arqSaida.write(f'Texto_1 & Texto_2......: {strResultadoAND}\n')
+arqSaida.write(f'Texto_1 | Texto_2......: {strResultadoOR}\n')
+arqSaida.write(f'Texto_1 ^ Texto_2......: {strResultadoXOR}\n')
+arqSaida.close()
